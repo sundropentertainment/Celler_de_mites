819c4c110d927954f40d1bf015180f25cff5d74e
lemontae <adrianoleonela@gmail.com>
2024-10-20 17:16:11 +0200

    Implement Phaser 3 correctly

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
+import Phaser from 'https://cdn.jsdelivr.net/npm/phaser@3/dist/phaser.js';
 import './style.css'
+
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: function() {
+            // Load assets here
+        },
+        create: function() {
+            // Create game elements here
+        },
+        update: function() {
+            // Game logic here
+        }
+    }
+};
+
+const game = new Phaser.Game(config);
